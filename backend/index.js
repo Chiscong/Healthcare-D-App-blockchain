f740363e37b56de6f6fd7ea03f2400cd2b2ddb0c
Chiscong <144198618+Chiscong@users.noreply.github.com>
2026-03-12 22:17:02 +0700

    fix: add url

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,7 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin: [
+      "https://healthcare-d-app-blockchain.vercel.app",
       "http://localhost:3000",
     ],
     methods: ["GET", "POST"],
